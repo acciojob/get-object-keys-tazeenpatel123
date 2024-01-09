ca327bb7d80e6f657b6602a050f7df3410f00638
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2024-01-09 22:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let student={
+	name="tazeen"
+}
+Object.prototype.getKeys=function {
+	return  Object.keys(this);//returns all the keys in obje
+	
+}
+console.log(student.getKeys());
